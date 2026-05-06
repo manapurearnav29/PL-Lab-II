1da193e33283293e2236a8c88715a46421500a25
manapurearnav29 <manapurearnav29@gmail.com>
2026-05-07 01:50:26 +0530

    Update dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,6 +1,4 @@
-student = {"roll_no": 101,
-"name": "Amit",
-"marks": 85}
+student = {"roll_no": 101,"name": "Amit","marks": 85}
 print("Dictionary:", student)
 print("Name:", student["name"])
 print("Marks:", student.get("marks"))
